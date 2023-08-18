cc818dc4c4596b13d885bf410fcf25fa5faa0ab3
Oleksandr Cherniavskyi <cherniavskyi.alexandr@linkdigital.com.au>
2023-08-18 10:40:19 +0300

    SXDEDPCXZIC-206 / Improve historical data view

--- ckanext/datavic_odp_theme/plugin.py
+++ ckanext/datavic_odp_theme/plugin.py
@@ -74,6 +74,7 @@ class DatavicXLoaderPlugin(xloaderPlugin):
         Do not touch proper values, because it will definitely break something."""
 
         resource_dict.setdefault("url_type", "datavic_xloader")
+        resource_dict.setdefault("format", "")
 
         super()._submit_to_xloader(resource_dict)
 
