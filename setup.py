e031bb6039bf9b3a89db1dea2ed98e27cd95e560
mutantsan <kvaqich@gmail.com>
2023-09-04 17:14:51 +0300

    SXDEDPCXZIC-217 / Setup Datapusher Plus

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         [ckan.plugins]
         datavic_odp_theme=ckanext.datavic_odp_theme.plugin:DatavicODPTheme
         datavic_odp_theme_auth=ckanext.datavic_odp_theme.plugin:DatavicODPThemeAuth
-        datavic_odp_xloader=ckanext.datavic_odp_theme.plugin:DatavicXLoaderPlugin
+        datavic_odp_datapusher=ckanext.datavic_odp_theme.plugin:DatavicDatapusherPlugin
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
