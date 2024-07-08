1fb18a0fbb00f9eda94556696a7c975f1154b764
alexmorev <aleksey.morev@linkdigital.com.au>
2024-07-08 20:23:04 +0300

    SXDEDPCXZIC-330 / fix columns rendering

--- ckanext/datavic_odp_theme/helpers.py
+++ ckanext/datavic_odp_theme/helpers.py
@@ -223,3 +223,34 @@ def datavic_get_dtv_url() -> str:
         url = url + "/"
 
     return url
+
+
+@helper
+def datastore_dictionary(resource_id: str, resource_view_id: str):
+    """
+    Return the data dictionary info for a resource
+    """
+    try:
+        resource_view = toolkit.get_action("resource_view_show")(
+            {},
+            {"id": resource_view_id}
+        )
+        headers = [
+            f for f in toolkit.get_action("datastore_search")(
+                {},
+                {
+                    "resource_id": resource_id,
+                    "limit": 0,
+                    "include_total": False
+                }
+            )["fields"]
+            if not f["id"].startswith("_")
+        ]
+
+        if "show_fields" in resource_view:
+            headers = [c for c in headers if c["id"] in resource_view["show_fields"]]
+
+        return headers
+
+    except (toolkit.ObjectNotFound, toolkit.NotAuthorized):
+        return []
